4767a89001fb4fc59457f31eacf3e45b1cd80aeb
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-06-11 20:26:34 +0200

    refactor(repo): separate topic

---
+++ src/main/java/com/femcoders/repositories/TopicRepository.java
@@ -0,0 +1,14 @@
+package com.femcoders.repositories;
+
+import com.femcoders.entities.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, Integer> {
+    Topic save(Topic topic);
+
+//    Optional<Topic> findByName(String name);
+}
