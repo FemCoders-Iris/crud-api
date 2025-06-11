7a96fba23bef5ea71bbebbd6a3f748ef38851b62
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-06-11 22:52:21 +0200

    feat(repo): add topic to search

--- src/main/java/com/femcoders/repositories/PhraseRepository.java
+++ src/main/java/com/femcoders/repositories/PhraseRepository.java
@@ -11,10 +11,21 @@ import java.util.List;
 
 @Repository
 public interface PhraseRepository extends JpaRepository<Phrase, Integer>, JpaSpecificationExecutor<Phrase> {
-    @Query("SELECT p FROM Phrase p WHERE " +
-            "LOWER(p.title) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
-            "LOWER(p.content) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
-            "LOWER(p.author) LIKE LOWER(CONCAT('%', :searchText, '%'))")
-    List<Phrase> findPhrasesBySearchText(@Param("searchText") String searchText);
+//    @Query("SELECT p FROM Phrase p JOIN p.topics t WHERE " +
+//            "LOWER(p.title) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
+//            "LOWER(p.content) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
+//            "LOWER(p.author) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
+//            "LOWER(t.name) LIKE LOWER(CONCAT('%', :searchText, '%'))")
+//    List<Phrase> searchPhrases(@Param("searchText") String searchText);
+    @Query(value = "SELECT p.* FROM phrases p " +
+            "LEFT JOIN phrase_topic pt ON p.id = pt.id " +
+            "LEFT JOIN topics t ON pt.id_topic = t.id_topic " +
+            "WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :searchText, '%')) " +
+            "OR LOWER(p.content) LIKE LOWER(CONCAT('%', :searchText, '%')) " +
+            "OR LOWER(p.author) LIKE LOWER(CONCAT('%', :searchText, '%')) " +
+            "OR LOWER(t.name) LIKE LOWER(CONCAT('%', :searchText, '%'))",
+            nativeQuery = true)
+    List<Phrase> searchPhrases(@Param("searchText") String searchText);
+
 
 }
