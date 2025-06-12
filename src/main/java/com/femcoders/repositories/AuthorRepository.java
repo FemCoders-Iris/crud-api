891db0d89d045275c28321a745e9fed9387a9944
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-06-12 12:37:52 +0200

    feat(AuthorRepo): add findByName method

--- src/main/java/com/femcoders/repositories/AuthorRepository.java
+++ src/main/java/com/femcoders/repositories/AuthorRepository.java
@@ -1,7 +1,12 @@
 package com.femcoders.repositories;
 
 import com.femcoders.entities.Author;
+import com.femcoders.entities.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    Optional<Author> findByName(String name);
 }
